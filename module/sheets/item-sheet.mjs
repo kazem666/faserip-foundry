7e8ac76262d9f80e5c16fa0aefa6ecd6dbd4e55a
kazem666 <36316412+kazem666@users.noreply.github.com>
2026-09-14 15:02:14 -0700

    1.13.0 item sheet factory for ActorSheetV2 / ItemSheetV2

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -1,33 +1,68 @@
 import { RANKS, BATTLE_EFFECTS } from "../config.mjs";
-import { getItemSheetClass } from "../foundry-api.mjs";
+import { getItemSheetV2, getItemSheetV1, getHandlebarsMixin } from "../foundry-api.mjs";
 
-const ItemSheetBase = getItemSheetClass();
+const TEMPLATE = "systems/faserip/templates/item/item-sheet.hbs";
 
-export class FaseripItemSheet extends ItemSheetBase {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["faserip", "sheet", "item"],
-      template: "systems/faserip/templates/item/item-sheet.hbs",
-      width: 520,
-      height: 560,
-      resizable: true,
-      submitOnChange: true
-    });
+function fillItemContext(sheet, context = {}) {
+  const item = sheet.document ?? sheet.item;
+  context.item = item;
+  context.system = item.system;
+  context.ranks = RANKS;
+  context.columns = Object.entries(BATTLE_EFFECTS).map(([id, col]) => ({ id, label: col.label }));
+  context.isPower = item.type === "power";
+  context.isTalent = item.type === "talent";
+  context.isContact = item.type === "contact";
+  context.isWeapon = item.type === "weapon";
+  context.isEquipment = item.type === "equipment" || item.type === "weapon";
+  context.cssClass = context.cssClass || (sheet.isEditable ? "editable" : "locked");
+  context.editable = sheet.isEditable;
+  return context;
+}
+
+export function buildItemSheetClass() {
+  const Mixin = getHandlebarsMixin();
+  const V2 = getItemSheetV2();
+  if (typeof Mixin === "function" && typeof V2 === "function") {
+    return class FaseripItemSheet extends Mixin(V2) {
+      static DEFAULT_OPTIONS = {
+        classes: ["faserip", "sheet", "item"],
+        tag: "form",
+        position: { width: 520, height: 560 },
+        window: { resizable: true, icon: "fa-solid fa-bolt" },
+        form: { submitOnChange: true, closeOnSubmit: false }
+      };
+
+      static PARTS = {
+        body: { template: TEMPLATE, scrollable: [""] }
+      };
+
+      async _prepareContext(options) {
+        const context = await super._prepareContext(options);
+        return fillItemContext(this, context);
+      }
+    };
   }
 
-  async getData(options) {
-    const context = await super.getData(options);
-    context.item = this.document ?? this.item;
-    context.system = context.item.system;
-    context.ranks = RANKS;
-    context.columns = Object.entries(BATTLE_EFFECTS).map(([id, col]) => ({ id, label: col.label }));
-    context.isPower = context.item.type === "power";
-    context.isTalent = context.item.type === "talent";
-    context.isContact = context.item.type === "contact";
-    context.isWeapon = context.item.type === "weapon";
-    context.isEquipment = context.item.type === "equipment" || context.item.type === "weapon";
-    context.cssClass = context.cssClass || "editable";
-    context.editable = this.isEditable;
-    return context;
+  const V1 = getItemSheetV1();
+  if (typeof V1 === "function") {
+    return class FaseripItemSheet extends V1 {
+      static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+          classes: ["faserip", "sheet", "item"],
+          template: TEMPLATE,
+          width: 520,
+          height: 560,
+          resizable: true,
+          submitOnChange: true
+        });
+      }
+
+      async getData(options) {
+        const context = await super.getData(options);
+        return fillItemContext(this, context);
+      }
+    };
   }
+
+  throw new Error("Foundry ItemSheetV2 / ItemSheet is not available");
 }
